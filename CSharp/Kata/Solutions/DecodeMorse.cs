ab2dc558e734122b14396614f814c2e3d67713f2
Qwxp <mmdjoker@yahoo.com>
2022-08-03 13:06:20 +0430

    .

--- CSharp/Kata/Solutions/DecodeMorse.cs
+++ CSharp/Kata/Solutions/DecodeMorse.cs
@@ -1,6 +1,7 @@
 ﻿namespace Kata.Solutions;
 
 //https://www.codewars.com/kata/54b724efac3d5402db00065e/train/csharp
+//https://www.codewars.com/users/MrAliSalehi/
 public class DecodeMorse : IKata
 {
 
